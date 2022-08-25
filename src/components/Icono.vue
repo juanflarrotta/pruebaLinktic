de54e1c5fc14d0502e6afbee5adddae9387c091a
olarrotta07 <oscarlarrotta@hotmail.com>
2022-08-25 09:54:56 -0500

    ajuste iconos

---
+++ src/components/Icono.vue
@@ -0,0 +1,17 @@
+<template>
+    <span :class="nameClass" v-html="icono">
+    </span>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: "Icono",
+
+    props: {
+        nameClass: '',
+        icono: '',
+    }
+}
+
+</script>
